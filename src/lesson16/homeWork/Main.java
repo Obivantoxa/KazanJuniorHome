0e3faf5f8e29e8c20bd472a429caebf30b05fd5c
Obivantoxa <Obivan.toxa@yandex.ru>
2022-07-31 11:21:38 +0300

    Kazan lesson 13 - homeWork 20.07.2022

---
+++ src/lesson16/homeWork/Main.java
@@ -0,0 +1,37 @@
+package lesson16.homeWork;
+
+import lesson16.Pevec2;
+import org.apache.log4j.Logger;
+
+public class Main {
+    /*
+    Создать программу как на уроке, толко уже с 3мя певцами.
+    Ведущий обьявил концерт.
+    Потом Должен спеть первый, затем второй, и третий певец .
+    Так три круга.
+    Ведущий закрыл шоу.
+    Так же подключить логирование и внести лог в файл.
+     Должен обязательно отображаться имя потока, проснулся, уснул
+     */
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Pevec1 pevec1 = new Pevec1();
+        Pevec2 pevec2 = new Pevec2();
+        Pevec3 pevec3 = new Pevec3();
+
+        pevec1.setName(" Поток 1 ");
+        pevec2.setName(" Поток 2 ");
+        pevec3.setName(" Поток 3 ");
+        pevec1.setDaemon(true);
+        System.out.println("hellow start");
+        pevec1.start();
+        pevec2.start();
+        pevec3.start();
+        while (pevec2.isAlive()) {
+
+        }
+        System.out.println("finish!!!");
+        LOGGER.info("Finish" + Thread.currentThread().getName());
+    }
+}
